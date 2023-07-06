ab972aa8ce9fde969c41f900393cc7871d7ec5db
chanwoo <chocw0402@gmail.com>
2023-07-07 02:25:04 +0900

    feat : 구간 합 구하기2

---
+++ untitled/src/main/java/org/example/boj/구간_합_구하기2.java
@@ -0,0 +1,19 @@
+package org.example.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 구간_합_구하기2 {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+
+    }
+}
