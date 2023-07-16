e18b74d884163858d23b11205a53bb48d9447925
chanwoo <chocw0402@gmail.com>
2023-07-17 00:30:54 +0900

    feat : 동전 - 그리디

---
+++ untitled/src/main/java/org/example/boj/동전0.java
@@ -0,0 +1,32 @@
+package org.example.boj;
+
+import java.util.Scanner;
+
+public class 동전0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int coinCount = scanner.nextInt();
+        int goal = scanner.nextInt();
+        int[] coins = new int[coinCount];
+        for (int i = 0; i < coinCount; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
+        int result = solution(goal, coins);
+        System.out.println(result);
+    }
+
+    private static int solution(int goalAmount, int[] coins) {
+        int count = 0;
+
+        for (int i = coins.length - 1; i >= 0; i--) {
+            if (goalAmount >= coins[i]) {
+                count += goalAmount / coins[i];
+                goalAmount = goalAmount % coins[i];
+            }
+        }
+
+        return count;
+    }
+}
