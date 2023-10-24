653ae03e3527d30c50829570cca5f2c6bb30bc6b
chanwoo <chocw0402@gmail.com>
2023-10-24 12:32:29 +0900

    feat : 추억 점수

---
+++ untitled/src/main/java/org/example/programmers/level1/추억_점수.java
@@ -0,0 +1,41 @@
+package org.example.programmers.level1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class 추억_점수 {
+    public static void main(String[] args) {
+        String[] name = {"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {
+                {"may", "kein", "kain", "radi"},
+                {"may", "kein", "brin", "deny"},
+                {"kon", "kain", "may", "coni"}
+        };
+        Solution solution = new Solution();
+        int[] result = solution.solution(name, yearning, photo);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> nameAndScore = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            nameAndScore.put(name[i], yearning[i]);
+        }
+
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (String n : photo[i]) {
+                Integer score = nameAndScore.getOrDefault(n, 0);
+                sum += score;
+            }
+            answer[i] = sum;
+        }
+
+        return answer;
+    }
+}
