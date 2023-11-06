821db192cd4654c1af1a869ee8183f07127c57b6
chanwoo <chocw0402@gmail.com>
2023-11-07 00:37:53 +0900

    feat : 콜라문제

--- untitled/src/main/java/org/example/programmers/level1/콜라문제.java
+++ untitled/src/main/java/org/example/programmers/level1/콜라문제.java
@@ -4,13 +4,19 @@ public class 콜라문제 {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		int result = solution.solution(2, 1, 20);
+		int result = solution.solution(3, 1, 20);
 		System.out.println(result);
 	}
 
 	static class Solution {
 		public int solution(int a, int b, int n) {
 			int answer = 0;
+			while ((n / a) != 0) {
+				int emptyBottle = n / a * a;
+				int received = emptyBottle / a * b;
+				answer += received;
+				n = n - emptyBottle + received;
+			}
 			return answer;
 		}
 	}
