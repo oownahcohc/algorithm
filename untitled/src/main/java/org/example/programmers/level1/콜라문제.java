9783951baa8d18d3723368c9689998fc71bb61ca
chanwoo <chocw0402@gmail.com>
2023-11-06 23:56:08 +0900

    feat : 콜라문제

---
+++ untitled/src/main/java/org/example/programmers/level1/콜라문제.java
@@ -0,0 +1,17 @@
+package org.example.programmers.level1;
+
+public class 콜라문제 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int result = solution.solution(2, 1, 20);
+		System.out.println(result);
+	}
+
+	static class Solution {
+		public int solution(int a, int b, int n) {
+			int answer = 0;
+			return answer;
+		}
+	}
+}
