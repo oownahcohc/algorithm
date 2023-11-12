e0b86aec90831da5855b7002473c47bee635d6cc
chanwoo <chocw0402@gmail.com>
2023-11-12 23:55:01 +0900

    feat : 최소 직사각형

---
+++ untitled/src/main/java/org/example/programmers/level1/최소직사각형.java
@@ -0,0 +1,29 @@
+package org.example.programmers.level1;
+
+public class 최소직사각형 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int result = solution.solution(new int[][] {
+			{14, 4},
+			{19, 6},
+			{6, 16},
+			{18, 7},
+			{7, 11}
+		});
+		System.out.println(result);
+	}
+
+	static class Solution {
+		public int solution(int[][] sizes) {
+			int w = 0;
+			int h = 0;
+			for (int[] cardSize : sizes) {
+				
+			}
+
+			int answer = 0;
+			return answer;
+		}
+	}
+}
